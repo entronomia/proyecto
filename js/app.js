4d6c56337c6b2c2ce88c08c17466e26f7a687d26
ropema <ropema@misena.edu.co>
2024-08-27 12:58:19 -0400

    version cero

---
+++ js/app.js
@@ -0,0 +1,45 @@
+function encriptar(){
+    let textoAEncriptar=document.getElementById('textoEntrada').value;
+    let entrada=textoAEncriptar.split('');
+    for (let j=0; j<entrada.length; j++){
+        let letra=entrada[j];
+        switch(letra){
+            case 'a':
+                entrada[j]='ai';
+                break;
+            case 'e':
+                entrada[j]='enter';
+                break;
+            case 'i':
+                entrada[j]='imes';
+                break;
+            case 'o':
+                entrada[j]='ober';
+                break;
+            case 'u':
+                entrada[j]='ufat';
+                break;
+            default:
+                entrada[j]=letra;
+        }
+    }
+    let salida=entrada.join('');
+    document.getElementById('textoSalida').value=salida;
+    return;
+}
+function desencriptar(){
+    let textoADesencriptar=document.getElementById('textoEntrada').value;
+    let entrada=textoADesencriptar;
+    entrada=entrada.replace(/ai/g,'a');
+    entrada=entrada.replace(/enter/g,'e');
+    entrada=entrada.replace(/imes/g,'i');
+    entrada=entrada.replace(/ober/g,'o');
+    entrada=entrada.replace(/ufat/g,'u');        
+    document.getElementById('textoSalida').value=entrada;
+    return;
+}
+function copiar(){
+    document.getElementById('textoEntrada').value="";
+    let textoACopiar=document.getElementById('textoSalida').value;
+    navigator.clipboard.writeText(textoACopiar);
+}
